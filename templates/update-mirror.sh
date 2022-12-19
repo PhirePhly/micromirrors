2ce402df0ef35f978ed389531b682c0be7d10866
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2022-12-18 22:01:33 -0800

    Added verbose to ubuntu module

--- templates/update-mirror.sh
+++ templates/update-mirror.sh
@@ -363,8 +363,8 @@ update_ubuntu() {
 	exec {lock_fd}>$LOCKDIR/mirror.ubuntu
 	flock $FLOCK_ARGS "$lock_fd" || return
 	echo -e "\n\n### UPDATING UBUNTU ###\n"
-	rsync  --recursive --times --links --safe-links --hard-links --stats --exclude "Packages*" --exclude "Sources*" --exclude "Release*" --exclude "InRelease" --bwlimit=25M rsync://$UPSTREAM/ubuntu/ /data/mirror/ubuntu/ &&\
-	rsync --recursive --times --links --safe-links --hard-links --stats --delete --delete-after --bwlimit=25M rsync://$UPSTREAM/ubuntu/ /data/mirror/ubuntu/
+	rsync  -v --recursive --times --links --safe-links --hard-links --stats --exclude "Packages*" --exclude "Sources*" --exclude "Release*" --exclude "InRelease" --bwlimit=25M rsync://$UPSTREAM/ubuntu/ /data/mirror/ubuntu/ &&\
+	rsync -v --recursive --times --links --safe-links --hard-links --stats --delete --delete-after --bwlimit=25M rsync://$UPSTREAM/ubuntu/ /data/mirror/ubuntu/
 	sleep 10
 	flock -u "$lock_fd"
 }
